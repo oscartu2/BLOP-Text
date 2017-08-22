70b61728557b59283736596f621ac84d726f27bf
Oscar Tu <o.car.w2@gmail.com>
2017-08-22 00:38:18 -0700

    Initial commit for  quote tracker project

---
+++ quoteTracker.py
@@ -0,0 +1,25 @@
+
+def refresh(interval):
+	# Updates the stocks and direction of the pricing with red/green arrows
+	# for every specified interval of time
+
+def search():
+	# Automatically webscrape stock information 
+
+def draw():
+	# Draw the GUI?
+	# GUI: Displays a list of stock symbols with current price, previous price, 
+	# and direction it has gone (up or down) since last refresh. 
+	# Possible also to show refreshed X times since launch, and other logistics.
+
+def prompt():
+	# Prompts users to enter in list of symbols of stocks and interval to refresh
+
+def main():
+	draw()
+	prompt()
+	search()
+
+
+if __name__ == "__main__":
+	main()
