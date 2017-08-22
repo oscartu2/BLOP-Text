b83ab0a3d70e410bdc8628cd8863f1299f5888a2
Oscar Tu <o.car.w2@gmail.com>
2017-08-22 00:42:27 -0700

    Start template and GUI

--- quoteTracker.py
+++ quoteTracker.py
@@ -1,24 +1,35 @@
+import tkinter
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
+
 
 def refresh(interval):
 	# Updates the stocks and direction of the pricing with red/green arrows
 	# for every specified interval of time
+	print "test"
 
 def search():
 	# Automatically webscrape stock information 
+	print "test search"
 
 def draw():
 	# Draw the GUI?
 	# GUI: Displays a list of stock symbols with current price, previous price, 
 	# and direction it has gone (up or down) since last refresh. 
 	# Possible also to show refreshed X times since launch, and other logistics.
+	print "test draw"
 
 def prompt():
 	# Prompts users to enter in list of symbols of stocks and interval to refresh
+	print "test prompt"
 
 def main():
 	draw()
 	prompt()
 	search()
+
+
 
 
 if __name__ == "__main__":
